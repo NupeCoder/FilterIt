23a285a2e3b7e855752110744d4fc296f4e28caf
Abubakar Liman <limanzaffar@gmail.com>
2025-02-12 11:50:33 +0000

    initial changes

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 // Background stuff
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === "sendText") {
-          // Send the message to the content script in the active tab
-          console.log(message)
-      }
-    });
+chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      files: ["content.js"]
+  });
+});
   
